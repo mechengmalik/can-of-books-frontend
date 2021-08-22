124d22ea5f9614645ba8f4bdaeeccdd18cb5540d
MechEngMalik <mecheng.malik93@gmail.com>
2021-08-22 18:02:52 +0300

    f

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
+import './Footer.css';
+
 
 class Footer extends React.Component {
   render() {
     return(
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>&copy; Best Books</Navbar.Brand>
+      <Navbar className="foot" collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand>&copy;2021 Best Books<br></br>MalikSwayyed</Navbar.Brand>
       </Navbar>
     );
   }
